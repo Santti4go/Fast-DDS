d9a21aab525ebf8361cc0ec45d3df252456a10e7
Santti4go <santiaupi@gmail.com>
2024-12-30 14:01:26 -0300

    Log any errors before cancel_init()
    Signed-off-by: Santti4go <santiaupi@gmail.com>

--- src/cpp/rtps/security/SecurityManager.cpp
+++ src/cpp/rtps/security/SecurityManager.cpp
@@ -119,9 +119,9 @@ bool SecurityManager::init(
         ParticipantSecurityAttributes& attributes,
         const PropertyPolicy& participant_properties)
 {
+    SecurityException exception;
     try
     {
-        SecurityException exception;
         domain_id_ = participant_->get_domain_id();
         const PropertyPolicy log_properties = PropertyPolicyHelper::get_properties_with_prefix(
             participant_->getRTPSParticipantAttributes().properties,
@@ -387,6 +387,10 @@ bool SecurityManager::init(
     {
         if (!e)
         {
+            // Unexpected code path. Let's log any errors
+            logError(SECURITY, "Error while configuring security plugin.")
+            if (strlen(exception.what())) logError(SECURITY, exception.what())
+
             cancel_init();
             return false;
         }
